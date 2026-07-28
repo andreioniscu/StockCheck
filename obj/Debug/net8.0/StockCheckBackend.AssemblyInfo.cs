7f4460d65c313217c3502cc2a06117b8acf4085a
Andrei Oniscu <andreioniscu@gmail.com>
2026-07-28 23:15:37 +0300

    docker

--- obj/Debug/net8.0/StockCheckBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/StockCheckBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockCheckBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f792905a1cb31e3b436c3b2da5df1f09ba8a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockCheckBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockCheckBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
